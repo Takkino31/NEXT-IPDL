721cb9ea3eaedb5ad5be0b764154ea0a441a5b8b
takkino31 <takkinoyaya@gmail.com>
2024-05-13 10:05:27 +0000

    NEXT IPDL DU 13 MAI 2024

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+// export default function Home() {
+//   return (
+//     <div className="flex h-screen w-screen items-center justify-center">
+//       <p className="text-bold">My Next Started App!</p>
+//     </div>
+//   );
+// }
+
+import HomePage from "../components/HomePage"
+
+
+const IndexPage = () =>{
+  return <HomePage/>
+}
+
+export default IndexPage
